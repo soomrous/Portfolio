3ca30da431b8897e2fd0c6f91964e7d2110f9c60
Sikander Soomro <49952081+soomrous@users.noreply.github.com>
2023-10-11 22:58:06 -0400

    Updated code

---
+++ old_scripts.js
@@ -0,0 +1,7 @@
+// var typed = new Typed(".text", {
+//     strings:["Project Management Professional (PMP)","Certified AWS Solution Architact" , "Certified AWS Solution Architact", "Full Stack Developer" , "Automation Testing Framework Engineer"],
+//     typeSpeed:100,
+//     backSpeed:100,
+//     backDelay:1000,
+//     loop:true
+// });
